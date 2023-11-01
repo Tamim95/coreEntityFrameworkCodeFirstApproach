970c8662c251db7605fc6f13de900021176ebef9
tamim.iqbal <tamim.cse14@gmail.com>
2023-11-01 09:13:59 +0600

    Add project files.

---
+++ coreEntityFrameworkCodeFirstApproach/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using coreEntityFrameworkCodeFirstApproach.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreEntityFrameworkCodeFirstApproach.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        public CustomerController()
+        {
+            _context = new ApplicationDBContext();
+        }
+
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+
+            var customers=_context.Customers.ToList();
+            return View(customers);
+            //var customers=_context.Customers.ToList() *** Customers model class picking data using ToList() in _context obj
+            // which is object of ApplicationDBContext model class. And all data storing ing customers variable.
+            //and Binding this customers variable with return View(customers). and generate a view here.
+
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ coreIdentityApplication/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreIdentityApplication.Controllers
+{
+    [AllowAnonymous]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Details() //  public IActionResult Details() => View();
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Policy()
+        {
+            return View();
+        }
+    }
+}
